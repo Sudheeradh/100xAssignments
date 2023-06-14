5e238a096590ba947b4a2a87767015776b53e990
sudheeradh <k.sudheeradh@gmail.com>
2023-06-14 14:45:55 +0530

    calc ⚠️

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,46 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    this.result += num;
+  }
+
+  subtract(num) {
+    this.result -= num;
+  }
+
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num === 0) {throw new Error("Can't divide by zero");}
+    else {this.result /= num};
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(str) {
+    str = str.replaceAll(" ","");
+
+    arr = []
+    let currnum = "";
+    for (let i = 0; i < str.length; ++i) {
+      if ()
+    }
+
+  }
+}
 
 module.exports = Calculator;
