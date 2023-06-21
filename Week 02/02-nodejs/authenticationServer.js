fd886edfe2f4cb6d381e5f6e572dde4fb4ea0d66
sudheeradh <k.sudheeradh@gmail.com>
2023-06-22 05:26:02 +0530

    authenticationServer ✅

--- Week 02/02-nodejs/authenticationServer.js
+++ Week 02/02-nodejs/authenticationServer.js
@@ -29,9 +29,89 @@
   Testing the server - run `npm run test-authenticationServer` command in terminal
  */
 
-const express = require("express")
+const express = require("express");
+const bodyParser = require('body-parser');
 const PORT = 3000;
 const app = express();
-// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+let users = []
 
+function isRepeated(fieldArg, fieldType) {
+  for(user of users) {
+    if (user[fieldType] === fieldArg) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function authenticate(email, password) {
+  for (user of users) {
+    if (user['email'] === email && user['password'] === password) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getData(loginDetails) {
+  for (user of users) {
+    if (user['email'] === loginDetails['email']) {
+      return user;
+    }
+  }
+}
+
+function handlePostSignup(req, res) {
+  newUser = req.body;
+
+  if (!("email" in newUser) || isRepeated(newUser.email, "email")) {
+    res.sendStatus(400);
+  } else {
+    currId = Math.floor(Math.random() * 100000);
+    while(isRepeated(currId, "id")) {
+      currId = Math.floor(Math.random() * 100000);
+    }
+    newUser['id'] = currId;
+    users.push(newUser);
+    res.status(201).send("Signup successful");
+  }
+}
+
+function handlePostLogin(req, res) {
+  bodyData = req.body;
+
+  if (authenticate(bodyData.email, bodyData.password)) {
+    userData = getData(bodyData);
+    userData["authentication token"] = Math.floor(Math.random() * 100000);
+    res.send(userData);
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+function handleGetData(req, res) {
+  let headerData = req.headers;
+
+  if (authenticate(headerData.email, headerData.password)) {
+    shareableUserData = [];
+    users.forEach(user => {
+      shareableUserData.push({
+        "email" : user.email,
+        "firstName" : user.firstName,
+        "lastName" : user.lastName,
+        "id" : user.id
+      })
+    })
+    res.send({"users" : shareableUserData});
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+app.use(bodyParser.json());
+app.post('/signup', handlePostSignup);
+app.post('/login', handlePostLogin);
+app.get('/data', handleGetData);
+
+// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
 module.exports = app;
